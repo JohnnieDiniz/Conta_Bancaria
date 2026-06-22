eaf2c1550f685b7dd6c49d15940f1345c7c623ea
João Vitor Diniz Alves <diniz.vitor.alves@gmail.com>
2026-06-22 13:17:54 -0300

    :sparkles: Métodos, Sacar/ despositar

--- conta_bancaria/src/conta_bancaria/MenuPrincipal.java
+++ conta_bancaria/src/conta_bancaria/MenuPrincipal.java
@@ -2,6 +2,7 @@ package conta_bancaria;
 
 import java.util.Scanner;
 
+import conta_bancaria.model.Conta;
 import conta_bancaria.util.Cores;
 
 public class MenuPrincipal {
@@ -9,9 +10,35 @@ public class MenuPrincipal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner leia = new Scanner(System.in);
+		
 		int opcao; 
 		
-		Scanner leia = new Scanner(System.in);
+		/* Instanciar Objetod da classe Conta */
+		
+		Conta c1 = new Conta(1,123, 1, "Isabela", 200000.00f);
+		c1.visualizar();
+		
+		Conta c2 = new Conta(2,123, 2, "Thiago", 100000.00f);
+		c2.visualizar();
+		
+		c1.setSaldo(300000.00f);
+		c1.visualizar();
+		
+		/*
+		 * if ternário 
+		 * 
+		 * condição ? ação se for verdadeiro : se for falso */
+		
+		System.out.println("\nSacar R$ 3000000,00 da conta C2: " + (c2.sacar(1000.00f) ?
+							"Saque efetuado com Sucesso" : "Saldo insuficiente"));
+		c2.visualizar();
+		
+		/* Deposito */
+		
+		c2.depositar(50000.00f);
+		c2.visualizar();
+		
 		
 		do {
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND);
@@ -38,27 +65,36 @@ public class MenuPrincipal {
 					System.out.print(" ---- Criar conta ----");
 					break;
 				case 2:
-					
+					leia.nextLine();
+					System.out.print(" ---- Listar todas as contas ----");
 					break;
 				case 3:
-					
+					leia.nextLine();
+					System.out.print(" ---- Buscar Contas por Números ----");
 					break;
 				case 4:
-					
+					leia.nextLine();
+					System.out.print(" ---- Criar conta ----");
 					break;
 				case 5:
-					
+					leia.nextLine();
+					System.out.print(" ---- Criar conta ----");
 					break;
 				case 6:
-					
+					leia.nextLine();
+					System.out.print(" ---- Criar conta ----");
 					break;
 				case 7:
-					
+					leia.nextLine();
+					System.out.print(" ---- Criar conta ----");
 					break;
 				case 8:
-					
+					leia.nextLine();
+					System.out.print(" ---- Criar conta ----");
 					break;
 				case 9:
+					leia.nextLine();
+					System.out.print(" ---- Criar conta ----");
 					System.out.println("Você saiu do programa! \n\nVolte sempre." );
 					break;
 					default:
@@ -67,12 +103,6 @@ public class MenuPrincipal {
 			
 		} while (opcao !=9);
 	
-		
-		
-		
-		
-		
-		
 		
 		leia.close();
 
